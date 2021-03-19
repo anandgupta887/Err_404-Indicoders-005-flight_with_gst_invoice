5fcaf5de4fdf6d8f6b83eb8685210fc7f44d9e78
Anand <anandgupta887@yahoo.com>
2021-03-19 18:57:51 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>All The Best</h1>
       <p>Hello World</p>
+      <p>Nallasopara</p>
     </div>
   );
 }

6d845e02a1309f538569a4632c109ee7a966d0c8
matin808 <matinkadri07@gmail.com>
2021-03-19 16:44:24 +0530

    Added Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
+import Home from "./assests/components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>All The Best</h1>
-      <p>Hello World</p>
-      <p>Nallasopara</p>
-      <p>Agla Station</p>
-      <p>Agla Station</p>
+      <Home />
     </div>
   );
 }

37599df8c83fa581c5ed7ba2bda8caa0641ce6bc
Nami24 <namisk8879@gmail.com>
2021-03-20 18:29:32 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from "./assests/components/Contact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Services from "./assests/components/Services";
 import Footer from "./assests/components/Footer";
+import ThankYou from "./assests/components/ThankYou";
 
 function App() {
   return (
@@ -18,18 +19,18 @@ function App() {
             <Header />
             <Contact />
           </Route>
-          <Route path="/thankyou">
-            <h1>This is thank you page</h1>
-          </Route>
           <Route path="/">
-            <Header home />
+            <Header />
             <Home />
             <Services />
+          </Route>
+          <Route path="/thankyou">
+            <ThankYou />
           </Route>
         </Switch>
         <Footer />
       </Router>
-      
+
     </div>
   );
 }

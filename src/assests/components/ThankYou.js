8de369321422da213ee6a046710a43969c1f516e
Nami24 <namisk8879@gmail.com>
2021-03-20 17:02:27 +0530

    added thankyou page

---
+++ src/assests/components/ThankYou.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ThankYou() {
+    return (
+    <div class="thank-you">
+        <div class="thanks">
+        <h1>Thank You</h1>
+        <a href="/" class="one-btn">close</a> 
+        </div>
+    </div>
+    )
+}
+
+export default ThankYou
+

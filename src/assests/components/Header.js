02d291050f3b9f6dc180f519b4969af1a96d748a
matin808 <matinkadri07@gmail.com>
2021-03-20 08:47:04 +0530

    style Header and Main page

--- src/assests/components/Header.js
+++ src/assests/components/Header.js
@@ -1,11 +1,28 @@
- import React from 'react'
- // import Airplane from "../images/airplane.jpg";
+
 import "../styles/Header.css";
 
  function Header() {
      return (
          <div>
-             
+        <div className="header">
+        <div className="left_side">
+          <img className = "header_logo" src="http://www.bitkraft.co.in/bk/wp-content/uploads/2014/06/TM-Bitkraft-Logo-PNG.png" alt=""/>
+        </div>
+
+      <div className="right_side">
+        <ul className = "header__links">
+          <li className = "header_link">
+            <a href="/" className = "header_a_link">Home </a>
+            <a href="/" className = "header_a_link">Book Flight</a>
+            <a href="/" className = "header_a_link">Search Invoices</a>
+            <a href="/" className = "header_a_link">Contact</a>
+           
+           
+          </li>
+        </ul>
+      </div>
+     
+      </div>
          </div>
      )
  }
